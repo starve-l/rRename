182f4373e2992d53868f8e9903e7587f5f9e5387
Delight_420 <68667474+Delight_420@users.noreply.github.com>
2021-02-12 18:29:41 +0100

    fixed config and bug

--- src/io/reliances/rrename/rRename.java
+++ src/io/reliances/rrename/rRename.java
@@ -28,28 +28,28 @@ public class rRename extends JavaPlugin {
     }
 
     @Override
-        public void onEnable() {
-            System.out.println("[rRename] Loading config...");
-            this.saveDefaultConfig();
-            PluginManager pm = getServer().getPluginManager();
-            getConfig().options().copyDefaults(getConfig().contains("version"));
-            getConfig().options().copyDefaults(getConfig().contains("appearance.prefix"));
-            getConfig().options().copyDefaults(getConfig().contains("appearance.prefix-color"));
-            getConfig().options().copyDefaults(getConfig().contains("appearance.color-normal"));
-            getConfig().options().copyDefaults(getConfig().contains("appearance.color-error"));
-            getConfig().options().copyDefaults(getConfig().contains("economy.enabled"));
-            getConfig().options().copyDefaults(getConfig().contains("economy.price"));
-            saveConfig();
-            rRenameListener listener = new rRenameListener();
-            pm.registerEvents(listener, this);
-            System.out.println("[rRename] Registering economy...");
-            setupEconomy();
-        }
+    public void onEnable() {
+        System.out.println("[rRename] Loading config...");
+        this.saveDefaultConfig();
+        PluginManager pm = getServer().getPluginManager();
+        getConfig().options().copyDefaults(getConfig().contains("version"));
+        getConfig().options().copyDefaults(getConfig().contains("appearance.prefix"));
+        getConfig().options().copyDefaults(getConfig().contains("appearance.prefix-color"));
+        getConfig().options().copyDefaults(getConfig().contains("appearance.color-normal"));
+        getConfig().options().copyDefaults(getConfig().contains("appearance.color-error"));
+        getConfig().options().copyDefaults(getConfig().contains("economy.enabled"));
+        getConfig().options().copyDefaults(getConfig().contains("economy.price"));
+        saveConfig();
+        rRenameListener listener = new rRenameListener();
+        pm.registerEvents(listener, this);
+        System.out.println("[rRename] Registering economy...");
+        setupEconomy();
+    }
 
-        @Override
-        public void onDisable() {  }
+    @Override
+    public void onDisable() {  }
 
-        FileConfiguration config = getConfig();
+    FileConfiguration config = getConfig();
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -67,75 +67,78 @@ public class rRename extends JavaPlugin {
                 player.sendMessage(prefix + colorNormal + "Version " + version + " by reliances.");
                 return true;
             }
-            }
+        }
 
-            switch (args[0]) {
-                case "reload":
-                    if (player.hasPermission("rrename.reload") || player.isOp()) {
-                        this.reloadConfig();
-                        config = this.getConfig();
-                        player.sendMessage(prefix + colorNormal + "Config successfully reloaded.");
-                    } else {
-                        player.sendMessage(prefix + colorError + "You don't have the required permission node: " + ChatColor.GRAY + "rrename.reload");
-                    }
-                    break;
-                case "info":
-                    player.sendMessage(prefix + colorNormal + "Version " + version + " by reliances.");
-                    break;
-                default:
-                        if (config.getBoolean("economy.enabled")) {
-                            if (!player.hasPermission("rrename.bypass") || !player.isOp()) {
-                                double bal = econ.getBalance(player);
-                                if (bal < config.getInt("economy.price")) {
-                                    player.sendMessage(prefix + colorError + "You have insufficient funds! You need $" + config.getInt("economy.price") + " to rename items.");
-                                    break;
-                                } else {
-                                    econ.withdrawPlayer(player, config.getInt("economy.price"));
-                                }
-                            }
-                        }
-                        String[] reSplitArgs;
-                        String finalArgs = "";
-                        String joinedArgs = "";
-                        for (int i = 0; i < args.length; i++) {
-                            joinedArgs += args[i] + " ";
-                        }
-                    joinedArgs = joinedArgs.substring(0, joinedArgs.length() - 1);
-                    reSplitArgs = joinedArgs.split("&#");
-                    finalArgs += reSplitArgs[0];
-                    for (int i = 1; i < reSplitArgs.length; i++) {
-                        String color = reSplitArgs[i].substring(0, 6);
-                        if (color.matches("^[A-Fa-f0-9]{6}"))
-                            finalArgs += ChatColor.of("#" + color) + reSplitArgs[i].substring(6);
-                        else {
-                            finalArgs += reSplitArgs[i].substring(6);
+        switch (args[0]) {
+            case "reload":
+                if (player.hasPermission("rrename.reload") || player.isOp()) {
+                    this.reloadConfig();
+                    config = this.getConfig();
+                    player.sendMessage(prefix + colorNormal + "Config successfully reloaded.");
+                } else {
+                    player.sendMessage(prefix + colorError + "You don't have the required permission node: " + ChatColor.GRAY + "rrename.reload");
+                }
+                break;
+            case "info":
+                player.sendMessage(prefix + colorNormal + "Version " + version + " by reliances.");
+                break;
+            default:
+                if (player.getInventory().getItemInMainHand().getType().isAir()) {
+                    player.sendMessage(prefix + colorError + "You need to hold an item/block to rename!");
+                }
+                if (config.getBoolean("economy.enabled")) {
+                    if (!player.hasPermission("rrename.bypass") || !player.isOp()) {
+                        double bal = econ.getBalance(player);
+                        if (bal < config.getInt("economy.price")) {
+                            player.sendMessage(prefix + colorError + "You have insufficient funds! You need $" + config.getInt("economy.price") + " to rename items.");
+                            break;
+                        } else {
+                            econ.withdrawPlayer(player, config.getInt("economy.price"));
                         }
                     }
-                            ItemStack is = player.getInventory().getItemInMainHand();
-                            ItemMeta im = is.getItemMeta();
-                            String formattedFinalArgs = ChatColor.translateAlternateColorCodes('&', finalArgs);
-                    assert im != null;
-                    im.setDisplayName(ChatColor.RESET + formattedFinalArgs);
-                            is.setItemMeta(im);
-                            player.updateInventory();
-                            player.sendMessage(prefix + colorNormal + "Item successfully renamed to " + ChatColor.RESET + formattedFinalArgs);
-            }
+                }
+                String[] reSplitArgs;
+                String finalArgs = "";
+                String joinedArgs = "";
+                for (int i = 0; i < args.length; i++) {
+                    joinedArgs += args[i] + " ";
+                }
+                joinedArgs = joinedArgs.substring(0, joinedArgs.length() - 1);
+                reSplitArgs = joinedArgs.split("&#");
+                finalArgs += reSplitArgs[0];
+                for (int i = 1; i < reSplitArgs.length; i++) {
+                    String color = reSplitArgs[i].substring(0, 6);
+                    if (color.matches("^[A-Fa-f0-9]{6}"))
+                        finalArgs += ChatColor.of("#" + color) + reSplitArgs[i].substring(6);
+                    else {
+                        finalArgs += reSplitArgs[i].substring(6);
+                    }
+                }
+                ItemStack is = player.getInventory().getItemInMainHand();
+                ItemMeta im = is.getItemMeta();
+                String formattedFinalArgs = ChatColor.translateAlternateColorCodes('&', finalArgs);
+                assert im != null;
+                im.setDisplayName(ChatColor.RESET + formattedFinalArgs);
+                is.setItemMeta(im);
+                player.updateInventory();
+                player.sendMessage(prefix + colorNormal + "Item successfully renamed to " + ChatColor.RESET + formattedFinalArgs);
+        }
         return true;
-        }
+    }
 
-        @Override
-        public List<String> onTabComplete (CommandSender sender, Command cmd, String label, String[]args){
-            List<String> completions = new ArrayList<>();
-            List<String> commands = new ArrayList<>();
+    @Override
+    public List<String> onTabComplete (CommandSender sender, Command cmd, String label, String[]args){
+        List<String> completions = new ArrayList<>();
+        List<String> commands = new ArrayList<>();
 
-            if (args.length == 1) {
-                if (sender.hasPermission("rrename.reload")) commands.add("reload");
-                if (sender.hasPermission("rrename.rename")) commands.add("[name]");
-                commands.add("info");
-                StringUtil.copyPartialMatches(args[0], commands, completions);
-            }
-            Collections.sort(completions);
-            return completions;
+        if (args.length == 1) {
+            if (sender.hasPermission("rrename.reload")) commands.add("reload");
+            if (sender.hasPermission("rrename.rename")) commands.add("[name]");
+            commands.add("info");
+            StringUtil.copyPartialMatches(args[0], commands, completions);
         }
+        Collections.sort(completions);
+        return completions;
+    }
 
 }
